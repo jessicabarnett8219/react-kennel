6a01d417d3d691c6c48ae3e1b2bfc754b5b07cca
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2018-11-18 11:41:41 -0600

    added animal component and used state and props to render

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import EmployeeList from "./employee/EmployeeList"
 import LocationList from "./location/LocationList"
+import AnimalList from "./animal/AnimalList"
 import "../Kennel.css"
 
 
@@ -11,25 +12,34 @@ class Kennel extends Component {
     { id: 2, name: "Jordan Nelson" },
     { id: 3, name: "Zoe LeBlanc" },
     { id: 4, name: "Blaise Roberts" }
-]
+  ]
 
-locationsFromAPI = [
-  { id: 1, name: "Nashville North", address: "500 Circle Way" },
-  { id: 2, name: "Nashville South", address: "10101 Binary Court" }
-]
+  locationsFromAPI = [
+    { id: 1, name: "Nashville North", address: "500 Circle Way" },
+    { id: 2, name: "Nashville South", address: "10101 Binary Court" }
+  ]
 
-state = {
-  employees: this.employeesFromAPI,
-  locations: this.locationsFromAPI
-}
+  animalsFromAPI = [
+    { id: 1, name: "Wally"},
+    { id: 2, name: "Andre"},
+    { id: 3, name: "Amigo"},
+    { id: 4, name: "Nelson"}
+  ]
+
+  state = {
+    employees: this.employeesFromAPI,
+    locations: this.locationsFromAPI,
+    animals: this.animalsFromAPI
+  }
 
 
   render() {
     return (
       <div>
         <h1>Student Kennels</h1>
-        <EmployeeList employees={this.state.employees}/>
-        <LocationList locations={this.state.locations}/>
+        <EmployeeList employees={this.state.employees} />
+        <LocationList locations={this.state.locations} />
+        <AnimalList animals={this.state.animals} />
       </div>
     );
   }
