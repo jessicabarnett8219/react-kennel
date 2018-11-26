87daf086cbc2eceb1d3d237f1b5877c2bf43c3ad
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2018-11-26 14:31:31 -0600

    finished chapter follow-along

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 import "./Animal.css"
 
 class AnimalList extends Component {
   render() {
-
     return (
       <section className="animals list">
         {
           this.props.animals.map(animal =>
             <div key={animal.id}>
               <h3>{animal.name}</h3>
-              <a href="#" onClick={() => this.props.deleteAnimal(animal.id)}>Delete</a>
+              <Link className="nav-link" to={`/animals/${animal.id}`}>Details</Link>
+              <a href="#" onClick={() => {
+                this.props.deleteAnimal(animal.id)
+              }}>Delete</a>
               <h5>Owned By:</h5>
               {this.props.getAnimalOwners(this.props.owners_animals, animal.id)}
             </div>
--- src/modules/AnimalManager.js
+++ src/modules/AnimalManager.js
@@ -1,10 +1,7 @@
 import APIManager from "./APIManager"
 
 class AnimalManager extends APIManager {
-  get() {
-    console.log("It worked")
-    this.get()
-  }
 }
 
 export default new AnimalManager("animals")
+
