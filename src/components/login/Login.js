5f0a2160aae372264dcdcfbc0695be578e215118
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2018-11-27 11:08:52 -0600

    added functionality to remember user or not and depending on answer, save to session or local storage

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -4,61 +4,67 @@ import React, { Component } from "react"
 
 export default class Login extends Component {
 
-    // Set initial state
-    state = {
-        email: "",
-        password: ""
+  // Set initial state
+  state = {
+    email: "",
+    password: "",
+    rememberMe: false
+  }
+
+  // Update state whenever an input field is edited
+  handleFieldChange = (evt) => {
+    const stateToChange = {}
+    stateToChange[evt.target.id] = evt.target.value
+    this.setState(stateToChange)
+  }
+
+  handleRememberBox = (evt) => {
+    const stateToChange = {}
+    stateToChange[evt.target.id] = evt.target.checked
+    this.setState(stateToChange)
+  }
+
+  handleLogin = (e) => {
+    e.preventDefault()
+    console.log(this.state.rememberMe)
+    if(this.state.rememberMe) {
+      localStorage.setItem(
+        "credentials",
+        JSON.stringify({
+          email: this.state.email,
+          password: this.state.password,
+        })
+      )
+    } else {
+      sessionStorage.setItem(
+        "credentials",
+        JSON.stringify({
+          email: this.state.email,
+          password: this.state.password,
+        })
+      )
     }
 
-    // Update state whenever an input field is edited
-    handleFieldChange = (evt) => {
-        const stateToChange = {}
-        stateToChange[evt.target.id] = evt.target.value
-        this.setState(stateToChange)
-    }
+  }
 
-    // Simplistic handler for login submit
-    handleLogin = (e) => {
-        e.preventDefault()
-
-        /*
-            For now, just store the email and password that
-            the customer enters into local storage.
-        */
-        sessionStorage.setItem(
-            "credentials",
-            JSON.stringify({
-                email: this.state.email,
-                password: this.state.password
-            })
-        )
-
-    }
-
-    render() {
-        return (
-            <form onSubmit={this.handleLogin} className="list">
-                <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-                <label htmlFor="inputEmail">
-                    Email address
-                </label>
-                <input onChange={this.handleFieldChange} type="email"
-                       id="email"
-                       placeholder="Email address"
-                       required="" autoFocus="" />
-                <label htmlFor="inputPassword">
-                    Password
-                </label>
-                <input onChange={this.handleFieldChange} type="password"
-                       id="password"
-                       placeholder="Password"
-                       required="" />
-                <button type="submit">
-                    Sign in
-                </button>
-            </form>
-        )
-    }
+  render() {
+    return (
+      <form onSubmit={this.handleLogin} className="list">
+        <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+        <label htmlFor="inputEmail">Email address</label>
+        <input onChange={this.handleFieldChange} type="email"
+          id="email"
+          placeholder="Email address"
+          required="" autoFocus="" />
+        <label htmlFor="inputPassword">Password</label>
+        <input onChange={this.handleFieldChange} type="password"
+          id="password"
+          placeholder="Password"
+          required="" />
+        <label>Remember Me</label>
+        <input onChange={this.handleRememberBox} type="checkbox" id="rememberMe" />
+        <button type="submit">Sign in</button>
+      </form>
+    )
+  }
 }
-
-{/* <Link className="nav-link" to={`/animals/${animal.id}`}>Details</Link> */}
